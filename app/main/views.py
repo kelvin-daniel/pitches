17cdb59808f3d74481d846282638605d639cf0b6
kelvin-daniel <kaymutor@gmail.com>
2020-09-20 18:44:15 +0300

    add index page views

---
+++ app/main/views.py
@@ -0,0 +1,15 @@
+
+from flask import render_template, redirect, url_for,abort,request
+from . import main
+from flask_login import login_required,current_user
+from ..models import User,Pitch,Comment,Upvote,Downvote
+from .form import UpdateProfile,PitchForm,CommentForm
+from .. import db,photos
+
+@main.route('/')
+def index():
+    pitches = Pitch.query.all()
+    job = Pitch.query.filter_by(category = 'Job').all() 
+    event = Pitch.query.filter_by(category = 'Events').all()
+    advertisement = Pitch.query.filter_by(category = 'Advertisement').all()
+    return render_template('index.html', job = job,event = event, pitches = pitches,advertisement= advertisement)
