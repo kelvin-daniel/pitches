299811a87c9432ce233df5958ba66d71d7bc18ce
kelvin-daniel <kaymutor@gmail.com>
2020-09-20 21:33:05 +0300

    add upvota and down vote clas

--- app/__init__.py
+++ app/__init__.py
@@ -6,14 +6,13 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import IMAGES, UploadSet,configure_uploads
 
-db = SQLAlchemy()
-mail = Mail()
 bootstap = Bootstrap()
+db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
-
+mail = Mail()
 
 
 def create_app(config_name):
---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kevon:37042490@localhost/pitches'
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = ("MAIL_USERNAME")
+    MAIL_PASSWORD = ("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kevon:37042490@localhost/pitches'
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
